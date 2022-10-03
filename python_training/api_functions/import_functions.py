c50a5992a9949bbf6fd57b08277311be3d2a42f0
Arie Twigt <ariekanarie@twigtd4.nl>
2022-10-03 13:03:21 +0200

    example code

--- python_training/api_functions/import_functions.py
+++ python_training/api_functions/import_functions.py
@@ -1,4 +1,5 @@
 import math
+import requests
 
 # Add a function, with type hinting
 def calculate_circle(diameter: float) -> float:
@@ -11,8 +12,18 @@ def calculate_circle(diameter: float) -> float:
     Returns:
     * Size of circle
     '''
+    
+    accepted_types = [int, float]
+
+    if type(diameter) not in accepted_types:
+        raise TypeError("The value should be an int or float")
 
     radius = diameter / 2
     size = radius ** 2 * math.pi
 
     return size
+
+
+# create a function that sends a request to the RDW API
+def get_cars_by_brand(brand: str) -> None:
+    pass
--- python_training/main.py
+++ python_training/main.py
@@ -1,13 +1,13 @@
-from api_functions.import_functions import calculate_circle
+from api_functions.import_functions import calculate_circle, get_cars_by_brand
 
 # the input will be a  'str' by default
 selected_diameter = input("Enter diameter:\n")
 
 # convert the 'str' variable to a 'float' data type
-selected_diameter_float = float(selected_diameter)
+#selected_diameter_float = float(selected_diameter)
 
 # insert the diameter value to our function
-circle_size = calculate_circle(selected_diameter_float)
+circle_size = calculate_circle(selected_diameter)
 
 # print the result
 print(circle_size)
