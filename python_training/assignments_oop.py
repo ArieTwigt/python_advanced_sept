b22955e3781b24a0d1818b1c3e4b8f641750b948
Arie Twigt <ariekanarie@twigtd4.nl>
2022-10-04 13:22:16 +0200

    added assignment for OOP

---
+++ python_training/assignments_oop.py
@@ -0,0 +1,6 @@
+from cars.cars import Car
+
+
+if __name__ == '__main__':
+    my_car = Car("NISSAN", "Micra", "RED", 1000)
+    pass
--- python_training/cars/cars.py
+++ python_training/cars/cars.py
@@ -3,13 +3,17 @@ from cars.garage import Garage
 class Car(Garage):
 
     # initial parameters of the class
-    def __init__(self, brand,
-                       model, 
-                       purpose="transport",
-                       availability="available"):
+    def __init__(self, brand: str,
+                       model: str, 
+                       color: str,
+                       price: float=0.0,
+                       purpose: str="transport",
+                       availability: str="available"):
                        
         self.brand = brand
         self.model = model
+        self.color = color 
+        self.price = price
         self.purpose = purpose
         self.availability = availability
         
@@ -17,6 +21,19 @@ class Car(Garage):
     # method for displaying the car
     def display(self):
         print(f"This is a {self.brand} of {self.model}")
+
+
+    # method for specifying the price of the car
+    def set_price(self, price: float):
+        self.price = price
+        print(f"setting the price to {price}")
+
+    
+    # method for increasing or decreasing the price of the car
+    def change_price(self, change_amount: float):
+        current_price = self.price
+        self.price += change_amount
+        print(f"The price changed from {current_price} to {self.price} (change of {change_amount)")
 
 
     # represent the clas in a formatted way
